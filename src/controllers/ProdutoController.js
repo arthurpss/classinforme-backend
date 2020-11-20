4a81174119a973db3c9487cc5d07f8d742cf297f
Arthur Passos <arthur.pcorrea@sempreceub.com>
2020-11-20 20:17:25 -0300

    Corrigindo query

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -4,7 +4,8 @@ module.exports = {
     async listaProdutosPorEmpresa(request, response) {
         const cnpj = request.params.cnpj;
         try {
-            const produtos = await connection('produto').select('*').from('produto').where('empresa_cnpj', cnpj);
+            const produtos = await connection('produto').where('empresa_cnpj', cnpj).select('*');
+            console.log(produtos)
             return response.json(produtos);    
         } catch (error) {
             console.log(error)
@@ -14,16 +15,21 @@ module.exports = {
     },
 
     async novoProduto(request, response) {
-        const empresa_cnpj = request.params.cnpj;
-        const { categoria, titulo, descricao, caminho_img, caminho_video } = request.body;
-        try {
-            await connection('produto').insert({
-                categoria, titulo, descricao, caminho_img, caminho_video, empresa_cnpj
-            });
-            return response.sendStatus(200);
-        } catch (error) {
-            console.log(error)
-            return response.sendStatus(400);
+        const empresa_param = request.params.cnpj;
+        const { categoria, titulo, descricao, caminho_img, caminho_video, empresa_cnpj } = request.body;
+        if (empresa_param === empresa_cnpj) {
+            try {
+                await connection('produto').insert({
+                    categoria, titulo, descricao, caminho_img, caminho_video, empresa_cnpj
+                });
+                return response.sendStatus(200);
+            } catch (error) {
+                return response.sendStatus(400);
+            }
         }
+        else {
+            return response.sendStatus(500);
+        }
+        
     }
 }
