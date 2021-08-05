811b9a62c19c96c3faf786da4957ac62b695eb9f
Arthur Passos <arthur.pcorrea@sempreceub.com>
2021-08-05 00:34:43 -0300

    Implementando painel de planos

---
+++ src/controllers/PlanoController.js
@@ -0,0 +1,77 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async listaPlanos(request, response) {
+        try {
+            const planos = await connection('plano').select('*');
+            return response.json(planos);
+        } catch (error) {
+            console.log(error)
+            return response.json(500)
+        }
+    },
+
+    async listaPlanoPorId(request, response) {
+        const id = request.params.id;
+        try {
+            const plano = await connection('plano').select('*').where('plano_id', id).first();
+            return response.json(plano);
+        } catch (error) {
+            console.log(error)
+            return response.json(500)
+        }
+    },
+
+    async listaPlanosAtivos(request, response) {
+        try {
+            const planos = await connection('plano').select('*').from('plano').where('ativo', 1);
+            return response.json(planos);
+        } catch (error) {
+            console.log(error)
+            return response.json(500)
+        }
+    },
+
+    async novoPlano(request, response) {
+        const { preco, titulo, descricao, ativo } = request.body;
+        try {
+            await connection('plano').insert({
+                preco, titulo, descricao, ativo
+            });
+            return response.sendStatus(200);
+        } catch (error) {
+            console.log(error)
+            return response.json(500)
+        }
+    },
+
+    async atualizaPlano(request, response) {
+        const id = request.params.id;
+        const { preco, titulo, descricao, ativo } = request.body;
+        try {
+            await connection('plano').where('plano_id', id).update({
+                preco: preco,
+                titulo: titulo,
+                descricao: descricao,
+                ativo: ativo
+            });
+            return response.sendStatus(200);
+        } catch (error) {
+            console.log(error)
+            return response.sendStatus(500);
+        }
+    }
+    /* 
+    async ativaDesativaPlano(request, response) {
+        const id = request.params.id;
+        const ativar = request.body.ativar;
+        try {
+            const plano = await connection('plano').where('plano_id', id).update('ativo', ativar);
+            // return response.sendStatus(200);
+            return response.json(plano);
+        } catch (error) {
+            console.log(error);
+            return response.sendStatus(400);
+        }
+    } */
+}
---
+++ src/database/migrations/20210731120950_plano.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('plano', function(table) {
+        table.increments('plano_id');
+        table.float('preco').notNullable();;
+        table.string('titulo').notNullable();
+        table.string('descricao').notNullable();;
+        table.boolean('ativo').notNullable();;
+      });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('plano');
+};
